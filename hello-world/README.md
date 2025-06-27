6538bb0629284fe3b1676a1edb6dcd35ca7b4f01
Isa <me.isa4@protonmail.com>
2025-06-27 23:24:08 +0300

    hello world

---
+++ hello-world/README.md
@@ -0,0 +1,1 @@
+Its a simple hellow world program in Golang
---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+package main // Its for calling this package as main packae
+import fmt // import fmt library which is required for print/input
+
+func main(){ // Th program will execute at this function
+	var hello_world string // We define a variable called Hello_World
+	fmt.Println(hello_world) // We printing hello_world variable
+}
