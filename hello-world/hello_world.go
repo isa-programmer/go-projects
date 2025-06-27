4eb9a259cfe3ef48c341b1b79545122f4cff10a8
Isa <me.isa4@protonmail.com>
2025-06-27 23:24:44 +0300

    fixed error

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -2,6 +2,6 @@ package main // Its for calling this package as main packae
 import fmt // import fmt library which is required for print/input
 
 func main(){ // Th program will execute at this function
-	var hello_world string // We define a variable called Hello_World
+	var hello_world string = "Hello, world!" // We define a variable called Hello_World
 	fmt.Println(hello_world) // We printing hello_world variable
 }
