1f4a1a773364886171888a1c20f44c722983f803
Isa <me.isa4@protonmail.com>
2025-07-01 23:13:08 +0300

    .

---
+++ projects/pwish/pwish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat pwish.txt
+
+echo -e "\033[37;45m \t\t\t\t                    Pwish-sh @v1.0.0                           \033[0m"
+echo -e "\033[35;47m \t\t\t\tAd: Pwish                                                      \033[0m"
+echo -e "\033[37;45m \t\t\t\tProgramlama dilleri: Java,Javascript,C,C++,C#,HTML,Python      \033[0m"
+echo -e "\033[35;47m \t\t\t\tKullandığı DE: KDE Plasma/XFCE4/Cinnamon                       \033[0m"
+echo -e "\033[37;45m \t\t\t\tOS: Gentoo                                                     \033[0m"
+echo -e "\033[35;47m \t\t\t\tProjeler: tampermonkey-beyler, drunkenspin                     \033[0m"
+echo -e "\033[37;45m \t\t\t\tGitlab: https://gitlab.com/pwish                               \033[0m"
+echo -e "\033[35;47m \t\t\t\t                    Pwish-sh @v1.0.0                           \033[0m"
+echo -e "\033[37;45m \t\t\t\t               Made by isa-programmer for pwish                \033[0m"
