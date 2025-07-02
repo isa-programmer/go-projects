e0e84ab0aae7d46525509d86773639234ce67502
Isa <me.isa4@protonmail.com>
2025-07-02 11:45:03 +0300

    Update install.sh

--- projects/pwish/install.sh
+++ projects/pwish/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "İndiriliyor..."
 
-FILE_URL="https://github.com/isa-programmer/go-projects/raw/refs/heads/main/projects/pwish/pwish-go"
-
-wget "$FILE_URL"
+wget "https://github.com/isa-programmer/go-projects/raw/refs/heads/main/projects/pwish/pwish-go"
 chmod +x pwish-go
 
 echo "İndirildi! Çalıştırmak için ./pwish-go"
